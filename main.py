24f024b58fe9bb7e2d2328b97f0daca45bba240c
Travis Lincoln <travis.dlincoln@gmail.com>
2026-01-14 15:11:39 -0800

    Updated main.py

--- main.py
+++ main.py
@@ -2,26 +2,33 @@
 #              without writing a lot of the boilerplate code for regular classes
 #              Automatically generate: __init__, __repr__, __eq__
 #              (Python 3.7)
+
 from dataclasses import dataclass, field
 
-# To make objects created from a data class immutable, use @dataclass (frozen=True)
-@dataclass
+# must add decorator @dataclass to declare a data class
+# (frozen=True) makes any object created from the data class immutable
+@dataclass (frozen=True)
 class Person:
+
+    # method __init__ is provided. We just need to provide data types
     name: str
     age: int
+
     # field used to customize how individual attributes behave
-    # password attribute will not be visible when object is printed
+    # password field will be hidden when objects are created
     password: str = field(repr=False)
     is_alive: bool = True
 
-    # After constructing the object
-    # The data class can call a __post__ method
-
+    # within a data class, the data class can automatically
+    # call the  __post__init__() method
+    # adds verification when trying to assign attributes
     def __post_init__(self):
         if self.age < 0:
-            raise ValueError("Age cannot be less than 0")
+            raise ValueError("Age cannot be negative")
 
 person1 = Person("Spongebob", 30, "pineapple1")
 person2 = Person("Patrick", 35, "password")
-person1.age = -1
+
+
+print(person1)
 print(person2)
